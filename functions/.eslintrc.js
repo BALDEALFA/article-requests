d8ce3998b2120d09798e2cba64f3566e22fa56ed
BALDE ALFA <alfaafro@gmail.com>
2021-03-26 22:27:51 +0000

    Async & Await

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -1,14 +1,14 @@
 module.exports = {
-  root: true,
-  env: {
-    es6: true,
-    node: true,
-  },
-  extends: [
-    "eslint:recommended",
-    "google",
-  ],
-  rules: {
-    quotes: ["error", "double"],
-  },
+	root: true,
+	env: {
+		es6: true,
+		node: true,
+	},
+	standard: {
+		parser: "babel-eslint",
+	},
+	extends: "eslint:recommended",
+	rules: {
+		quotes: ["error", "double"],
+	},
 };
